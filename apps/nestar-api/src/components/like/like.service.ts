5cb28e0a98b3c39fccfe146b67e4d37c559755ca
BahodirL7 <bakhodirtursunovl7@gmail.com>
2025-02-17 11:54:05 +0900

    BRR: Initial Setup...

---
+++ apps/nestar-api/src/components/like/like.service.ts
@@ -0,0 +1,79 @@
+import { BadGatewayException, BadRequestException, Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, ObjectId } from 'mongoose';
+import { Like, MeLiked } from '../../libs/dto/like/like';
+import { LikeInput } from '../../libs/dto/like/like.input';
+import { T } from '../../libs/types/common';
+import { Message } from '../../libs/enums/common.enum';
+import { OrdinaryInquiry } from '../../libs/dto/property/property.input';
+import { Properties } from '../../libs/dto/property/property';
+import { LikeGroup } from '../../libs/enums/like.enum';
+import { lookupFavorite } from '../../libs/config';
+
+@Injectable()
+export class LikeService {
+	constructor(@InjectModel('Like') private readonly likeModel: Model<Like>) {}
+
+	public async toggleLike(input: LikeInput): Promise<number> {
+		const search: T = { memberId: input.memberId, likeRefId: input.likeRefId };
+		const existing = await this.likeModel.findOne(search).exec();
+		let modifier: number;
+
+		if (existing) {
+			await this.likeModel.findOneAndDelete(search).exec();
+			modifier = -1; // Unlike
+		} else {
+			try {
+				await this.likeModel.findOneAndUpdate(search, input, { upsert: true, setDefaultsOnInsert: true }).exec();
+				modifier = 1; // Like
+			} catch (err) {
+				console.error('Error, Service.model', err.message);
+				throw new BadRequestException(Message.CREATE_FAILED);
+			}
+		}
+
+		console.log(`- Like modifier ${modifier} -`);
+		return modifier;
+	}
+
+	public async checkLikeExistence(input: LikeInput): Promise<MeLiked[]> {
+		const { memberId, likeRefId } = input;
+		const result = await this.likeModel.findOne({ memberId: memberId, likeRefId: likeRefId }).exec();
+
+		return result ? [{ memberId: memberId, likeRefId: likeRefId, myFavorite: true }] : [];
+	}
+	public async getFavoriteProperties(memberId: ObjectId, input: OrdinaryInquiry): Promise<Properties> {
+		const { page, limit } = input;
+		const match: T = { likeGroup: LikeGroup.PROPERTY, memberId: memberId };
+		const data: T = await this.likeModel
+			.aggregate([
+				{ $match: match },
+				{ $sort: { updatedAt: -1 } },
+				{
+					$lookup: {
+						from: 'properties',
+						localField: 'likeRefId',
+						foreignField: '_id',
+						as: 'favoriteProperty',
+					},
+				},
+				{ $unwind: '$favoriteProperty' },
+				{
+					$facet: {
+						list: [
+							{ $skip: (page - 1) * limit },
+							{ $limit: limit },
+							lookupFavorite,
+							{ $unwind: '$favoriteProperty.memberData' },
+						],
+						metaCounter: [{ $count: 'total' }],
+					},
+				},
+			])
+			.exec();
+		const result: Properties = { list: [], metaCounter: data[0].metaCounter };
+		result.list = data[0].list.map((ele) => ele.favoriteProperty);
+		console.log('result:', result);
+		return result;
+	}
+}
